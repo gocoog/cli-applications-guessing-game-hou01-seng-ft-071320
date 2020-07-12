80446f0218e31adeea1b59a1ae0ade8a0322ef43
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-12 06:41:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
   if guess.to_i == number
     puts "You guessed the correct number!"
   elsif guess.to_i != number
-    puts "Sorry! The computer guessed #{number}"
+    puts "Sorry! The computer guessed #{number}."
   end
   if guess == "exit"
     puts "Goodbye!"
